56184333a769450a5b0802b374e14b810c4236f5
MaliaRK <maliaraees2@gmail.com>
2025-03-08 00:13:33 +0500

    update complete code

--- app.py
+++ app.py
@@ -1,112 +1,97 @@
-import streamlit as st
 import random
 import string
-import re
-import bcrypt
+import streamlit as st
 
 st.set_page_config(page_title="Password_Strength_Meter", page_icon="🔐", layout="wide")
 
 st.markdown(
-    """
+    f"""
     <style>
-    .stApp {
-        background-color: #b4b4b4;
-    }
+    .stApp {{
+        background-color: whitesmoke;
+        margin: 0 20%;
+    }}
+
+    .stSlider > div > div > div > div > div {{
+        color: rgb(39, 38, 37);
+    }}
+
+    .stSlider > div > div > div > div {{
+        background-color: gray;
+    }}
     </style>
     """,
     unsafe_allow_html=True
 )
 
-st.title("Make Your own password...!")
-password_list = []
+st.title("Password_Strength_Meter")
 
-def password_generator(password):
+length = st.slider(label="Length", min_value=8, max_value=25)
+uppercase = st.checkbox(label="Uppercase")
+lowercase = st.checkbox(label="Lowercase")
+digits = st.checkbox(label="Digits")
+special_characters = st.checkbox(label="Special haracters (!@#$%^&*_)")
+
+def generate_password(length, uppercase, lowercase, digits, special_characters):
     score = 0
-    st.session_state.length = len(password)
-    blacklist = ['password123', 'abc123', '123456789']
+    characters = string.whitespace
+    color = ""
 
-    if password.lower() in blacklist:
-        st.error('This is a common password. Please choose another one.')
-        return
-
-    if st.session_state.length >= 8:
+    if uppercase:
+        characters += string.ascii_uppercase
         score += 1
-    else:
-        st.warning("password should be at least 8 characters long.")
-
-    if re.search(r"[A-Z]", password) and re.search(r"[a-z]", password):
+   
+    if lowercase:
+        characters += string.ascii_lowercase
         score += 1
-    else:
-        st.warning("password should contain capital and small letters.")
 
-    if re.search(r"\d", password):
-        score += 1
-    else:
-        st.warning("password should include atleast one digit (0-9).")
-
-    if re.search(r"[!@#$%^&*_]", password):
-        score += 1
-    else:
-        st.warning("password should have one special character (!@#$%^&*_).")
-         
-        if score > 0 and score <= 2:
-            st.error("weak password! Make a strong password using above suggesstion.")
-        elif score == 3:
-            st.error("Good but add some security features.")
-        
-    if score == 4:
-        st.success("strong password.")
-
-
-password = st.text_input(label="Enter  Password: ", value="", key="password", on_change= None)
-password_generator(password)
-
-# password encryption
-password_bytes = password.encode('utf-8')
-salt = bcrypt.gensalt()
-hashed_password = bcrypt.hashpw(password_bytes, salt)
-
-
-re_enter_password = st.text_input(label="Re-Enter Password: ", key='re_enter')
-
-re_enter_bytes = re_enter_password.encode('utf-8')
-
-match = bcrypt.checkpw(re_enter_bytes, hashed_password)
-
-if re_enter_password != "":
-    if match:
-        st.success('password matched!')
-    else:
-        st.error('password do not match')
-
-button_disable = not(password and match)
-
-if st.button(label='Generate Password', disabled=button_disable):
-    if button_disable == False:
-        st.subheader(f"Password generated: {password}")
-
-st.write('-------------------------------------------------------')
-
-
-st.title("Password Generator...!")
-
-def generate_password(length, use_digits, use_special):
-    characters = string.ascii_letters
-
-    if use_digits:
+    if digits:
         characters += string.digits
+        score += 1
 
-    if use_special:
+    if special_characters:
         characters += string.punctuation
+        score += 1
+
+    if uppercase and lowercase and digits and special_characters:
+        st.success("Strong password")
+    elif uppercase and lowercase and digits:
+        st.warning("Moderate password.. add special_characters")
+    elif uppercase and lowercase and special_characters:
+        st.warning("Common password.. add digits")
+    elif not uppercase and not lowercase and not digits and not special_characters:
+        st.warning("Please select above features.")
+    else:
+        st.warning("Weak password.. use above features.")
+
+    total = (score / 4) * 100
+
+    if total == 100:
+        color = "green"
+    elif total == 75:
+        color = "yellow"
+    elif total == 50:
+        color = "orange"
+    elif total == 25:
+        color = "red"
+
+    st.markdown(
+        f"""
+            <style>
+            .stProgress > div > div > div > div {{
+                background-color: {color};              
+            }}
+            </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+
+    st.progress(int(total))
+    st.write(f"Password Strength: {int(total)}%")
 
     return "".join(random.choice(characters) for _ in range(length))
 
-length = st.slider(label='Select password length:', min_value=6, max_value=25, value=8)
-
-use_digits =  st.checkbox(label='Include numbers')
-
-use_special = st.checkbox(label='Include special characters')
-
-if st.button(label='Generate Password'):
-    password = generate_password(length, use_digits, use_special)
-    st.write(f"Generated Password: `{password}`")
+if st.button(label="Generate Password"):
+    password = generate_password(length, uppercase, lowercase, digits, special_characters)
+    st.write(f"Your generated password is: {password}")
